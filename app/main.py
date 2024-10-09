c4fae1843686a5c6f24290e1072155e59ac36fad
leovasc5 <leovasc5@hotmail.com>
2024-10-09 18:44:20 -0300

    cleaned unecessary files

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 import os
 import streamlit as st
 import zipfile
+from utils.student import Student
 from interface.layout import render_layout
 from services.ai_generator import process_feedback
-from services.database import execute_query
-from utils.student import Student
-from utils.validators import validate_query
-from utils.plot import generate_plot
 
 def main():
     st.title("Feedback de Exercícios de SQL com IA Generativa")
@@ -24,7 +21,7 @@ def main():
                 students = extract_students(uploaded_file)
                 create_feedback(question_input, students)
 
-
+                st.success("Feedbacks gerados com sucesso! Você pode baixar um PDF com os feedbacks na parte superior direita da tela.")
 
 def extract_students(zip_file):
     students = []
@@ -37,7 +34,6 @@ def extract_students(zip_file):
                 with zip_ref.open(file_name) as sql_file:
                     sql_content = sql_file.read().decode("utf-8")
                     
-                    # Cria um objeto Student e adiciona à lista
                     student = Student(name=student_name, script=sql_content)
                     students.append(student)
     return students
@@ -47,31 +43,6 @@ def create_feedback(question_input, students):
         print(student)
         with st.spinner(f"Corrigindo exercício do aluno {student.get_name()}..."):
             process_feedback(question_input, student)
-        
-
-    # Processa a entrada do usuário
-    # if question_input:
-    #     with st.spinner('Gerando query...'):
-    #         # Gera a query a partir do prompt do usuário usando a IA
-    #         generated_query, chart_config = generate_query(question_input)
-
-    #     if generate_query:
-    #         st.subheader("Query Gerada")
-    #         st.code(generated_query, language='sql')
-            
-    #         # Valida a query gerada
-    #         if validate_query(generated_query):
-    #             # Executa a query no banco de dados
-    #             results = execute_query(generated_query)
-    #             plot = generate_plot(results, chart_config)
-                
-    #             # Exibe os resultados
-    #             st.subheader("Resultados")
-    #             st.write(results)
-    #             if plot:
-    #                 st.pyplot(plot)
-    #         else:
-    #             st.error("A query gerada não é válida. Tente reformular a pergunta.")
     
 if __name__ == "__main__":
     main()
--- app/services/ai_generator.py
+++ app/services/ai_generator.py
@@ -1,25 +1,21 @@
 import ollama
-from utils.files import read_file
 from utils.student import Student
-from services.database import get_db_structure
-import re
-import unidecode
+from utils.files import read_file
 import streamlit as st
 
-# Configurações da API
 MODEL_NAME = 'codellama'
 PROMPT_TEMPLATE = read_file("app/prompts/prompt_template.txt")
         
 def process_feedback(question: str, student: Student):
     """
-    Gera o feedback para o aluno usando IA generativa e atualiza o feedback no objeto Student.
-    
+    Generate the feedback for the student using generative AI and update the feedback in the Student object.
+
     Args:
-        question (str): A pergunta que será usada como instrução para o modelo de IA.
-        std (Student): O objeto Student contendo o script do aluno e outros dados.
+        question (str): The question that will be used as instruction for the AI model.
+        std (Student): The object that contains the student information.
         
     Returns:
-        None: O feedback é salvo diretamente no objeto Student.
+        None: The feedback is setted in the Student object.
     """
     try:
         prompt = PROMPT_TEMPLATE.format(
@@ -46,54 +42,3 @@ def process_feedback(question: str, student: Student):
     
     except Exception as e:
         st.error(f"Erro ao processar o feedback: {e}")
-
-
-def generate_query(user_input: str) -> str:
-    """
-    Gera uma query SQL a partir da entrada do usuário usando IA generativa.
-    
-    Args:
-        user_input (str): A consulta em linguagem natural fornecida pelo usuário.
-        
-    Returns:
-        str: A query SQL gerada pelo modelo de IA.
-    """
-    try:
-        isCorrect = False
-        prompt_template = read_file("app/prompts/prompt_template.txt")
-        db_structure = get_db_structure()
-
-        if not db_structure:
-            return None, None
-        
-        prompt = prompt_template.format(
-            instruction=user_input,
-            db_structure=db_structure
-        )
-        
-        while not isCorrect:
-            # Chama a API do modelo de IA para gerar a query
-            response = ollama.generate(
-                model=MODEL_NAME,
-                prompt=prompt,
-            )
-
-            print(response.get('response'))
-
-            query, chart_type, y_chart_value, x_chart_value = extract_values(response.get('response'))
-            chart_config = {
-                "chart_type": chart_type, 
-                "y_chart_value": y_chart_value,
-                "x_chart_value": x_chart_value
-            }
-
-            if query:
-                isCorrect = True
-
-            print(chart_config)
-        
-        return query, chart_config
-    
-    except Exception as e:
-        st.error(f"Erro ao gerar a query: {e}")
-        return ""
